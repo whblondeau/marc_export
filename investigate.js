8bd4031b61779767475a3d84470ddecb85aa59a1
Bill Blondeau <whblondeau@gmail.com>
2016-08-10 08:58:07 -0500

    initial content from working dir

---
+++ investigate.js
@@ -0,0 +1,239 @@
+
+
+var albums_json = 
+[
+    {
+        "album": {
+            "id": "byron-au-yong-piano-concerto-houston",
+            "asset_type": "album",
+            "title": "Piano Concerto–Houston",
+            "front_cover_art": "https://playback-jsfs.spl.org/complete-submission/albums/byron-au-yong-piano-concerto-houston/bandcamp.jpg",
+            "main_artist": "byron-au-yong",
+            "main_artist_name": "Byron Au Yong",
+            "release_date": "2014-04-11T00:00:00.000Z",
+            "record_label": "Independent",
+            "upc": null,
+            "genre": "Classical",
+            "spoken_languages": null,
+            "tracks": [
+                {
+                    "title": "Sparse",
+                    "filename": "01_Sparse.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/01_sparse--1468096254000.mp3",
+                    "position": 1,
+                    "duration": 157.5710090702948,
+                    "access_token": "9f1cf02b71226e3b8251735fd390c50e056c894e"
+                },
+                {
+                    "title": "Intimate",
+                    "filename": "02_Intimate.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/02_intimate--1468096288000.mp3",
+                    "position": 2,
+                    "duration": 177.0583560090703,
+                    "access_token": "58b73631f83d68d715f4c941fdfbcc06f0c4d783"
+                },
+                {
+                    "title": "Ethereal",
+                    "filename": "03_Ethereal.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/03_ethereal--1468096325000.mp3",
+                    "position": 3,
+                    "duration": 481.0714172335601,
+                    "access_token": "b3923ce2381bb2efde58e6d5b09cc4d981856153"
+                },
+                {
+                    "title": "Dark",
+                    "filename": "04_Dark.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/04_dark--1468096385000.mp3",
+                    "position": 4,
+                    "duration": 247.64121315192745,
+                    "access_token": "7ede6c624e714f59b6901f61f41d2e5dccd34896"
+                },
+                {
+                    "title": "Curious",
+                    "filename": "05_Curious.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/05_curious--1468096434000.mp3",
+                    "position": 5,
+                    "duration": 60.13427437641723,
+                    "access_token": "b2e76fc5f5f64a96e50a4a419a652159dbda3834"
+                },
+                {
+                    "title": "Gift",
+                    "filename": "6 Gift.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/6-gift--1468097034000.mp3",
+                    "position": 6,
+                    "duration": 263.10592970521543,
+                    "access_token": "842c65e1c2b6ef6e36d9a70972510275be7a97d3"
+                },
+                {
+                    "title": "Refined",
+                    "filename": "07_Refined.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/07_refined--1468097070000.mp3",
+                    "position": 7,
+                    "duration": 308.2714172335601,
+                    "access_token": "9b81a37962c8eeb267d479510ead9865a7a47b29"
+                },
+                {
+                    "title": "Open",
+                    "filename": "08_Open.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/08_open--1468097088000.mp3",
+                    "position": 8,
+                    "duration": 68.15386621315193,
+                    "access_token": "1336d986c7af73d981681fea62658f5ab1b83ca9"
+                },
+                {
+                    "title": "Playful",
+                    "filename": "09_Playful.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/09_playful--1468097126000.mp3",
+                    "position": 9,
+                    "duration": 238.99468253968254,
+                    "access_token": "ad25c51232228c7428111a0ef5a6b5db956bfa8d"
+                },
+                {
+                    "title": "Calm",
+                    "filename": "10_Calm.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/10_calm--1468097157000.mp3",
+                    "position": 10,
+                    "duration": 190.30243764172334,
+                    "access_token": "deb00f55a64eb8cb4cb941410f631381d819493c"
+                },
+                {
+                    "title": "Resonant",
+                    "filename": "11_Resonant.mp3",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/byron-au-yong-piano-concerto-houston/11_resonant--1468097173000.mp3",
+                    "position": 11,
+                    "duration": 221.8061111111111,
+                    "access_token": "16a1a94f5376c3651e41311cc2cfcc332d59d3c0"
+                }
+            ]
+        }
+    },
+    {
+        "album": {
+            "id": "sol-de-noche-endiablada-detangos",
+            "asset_type": "album",
+            "title": "Endiablada deTangos, Mirta Wymerszberg with Sol de Noche and friends",
+            "front_cover_art": "https://playback-jsfs.spl.org/complete-submission/albums/sol-de-noche-endiablada-detangos/endiablada-front-cd-cover.jpg",
+            "main_artist": "sol-de-noche",
+            "main_artist_name": "Sol de Noche",
+            "release_date": "2016-03-01T08:00:00.000Z",
+            "record_label": "Independent",
+            "upc": null,
+            "genre": "World Music",
+            "spoken_languages": "Spanish",
+            "tracks": [
+                {
+                    "title": "Duwamish river",
+                    "filename": "07 Duwamish river.wav",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/07-duwamish-river--1468270513000.wav",
+                    "position": 7,
+                    "duration": 0,
+                    "access_token": "4e2cf3f94308a263b40fafbdca1cf639079b3b77"
+                },
+                {
+                    "title": "La Endiablada ",
+                    "filename": "01 La Endiablada 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/01-la-endiablada-1--1468270942000.aiff",
+                    "position": 1,
+                    "duration": 135.78706349206348,
+                    "access_token": "b018c4e4fa27f384d98bf585ec62292ec45d6c8c"
+                },
+                {
+                    "title": "LaBordona",
+                    "filename": "2 LaBordona.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/2-labordona--1468271080000.aiff",
+                    "position": 2,
+                    "duration": 242.88039682539682,
+                    "access_token": "c3966f94579aafb3e578670835d13c935760320b"
+                },
+                {
+                    "title": "Absurdo",
+                    "filename": "03 Absurdo.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/03-absurdo--1468271088000.aiff",
+                    "position": 3,
+                    "duration": 162.40039682539683,
+                    "access_token": "db4b0d9fa53f4af41811fe6e44bba654265d31ba"
+                },
+                {
+                    "title": "El Andariego ",
+                    "filename": "04 El Andariego 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/04-el-andariego-1--1468271095000.aiff",
+                    "position": 4,
+                    "duration": 211.0403968253968,
+                    "access_token": "856fef6fbb79759453aec6a7bf0abdad94794523"
+                },
+                {
+                    "title": "Callejera ",
+                    "filename": "05 Callejera 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/05-callejera-1--1468271103000.aiff",
+                    "position": 5,
+                    "duration": 142.0403968253968,
+                    "access_token": "f5072c6b2919aaed647074dc30cc2412141e57ba"
+                },
+                {
+                    "title": "La Cachila ",
+                    "filename": "06 La Cachila 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/06-la-cachila-1--1468271114000.aiff",
+                    "position": 6,
+                    "duration": 194.8670634920635,
+                    "access_token": "ee2dba18a49d7dbef5e4ceeee29b4e631867e9fd"
+                },
+                {
+                    "title": "Dominguera",
+                    "filename": "08 Dominguera.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/08-dominguera--1468271120000.aiff",
+                    "position": 8,
+                    "duration": 156.81373015873015,
+                    "access_token": "b7ad5b84013888a2e752ceb95baeab9ed9a36e23"
+                },
+                {
+                    "title": "Arrabal",
+                    "filename": "09 Arrabal.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/09-arrabal--1468271129000.aiff",
+                    "position": 9,
+                    "duration": 287.16039682539684,
+                    "access_token": "6d61a13f3467abdee044d3c61d95355229bc9afd"
+                },
+                {
+                    "title": "A los Amigos ",
+                    "filename": "11 A los Amigos 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/11-a-los-amigos-1--1468271134000.aiff",
+                    "position": 11,
+                    "duration": 315.6403968253968,
+                    "access_token": "9be0c654b4f688a9d4862594a5e955b7d59422f3"
+                },
+                {
+                    "title": "TupalidaVoz",
+                    "filename": "12TupalidaVoz.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/12tupalidavoz--1468272085000.aiff",
+                    "position": 12,
+                    "duration": 202.7470634920635,
+                    "access_token": "480be6e2319298f096033759af53bc653d54886b"
+                },
+                {
+                    "title": "La Luciernaga ",
+                    "filename": "10 La Luciernaga 1.aiff",
+                    "url": "https://playback-jsfs.spl.org/complete-submission/sol-de-noche-endiablada-detangos/10-la-luciernaga-1--1468356707000.aiff",
+                    "position": 10,
+                    "duration": 179.6003968253968,
+                    "access_token": "d8adba6f6d0ffa92400ab639202ca8defb28d0c5"
+                }
+            ]
+        }
+    }
+];
+
+
+document.writeln('\nMARC export of example content\n');
+
+marc_albums = [];
+
+for (var doodad in albums_json) {
+
+    // we need WINDOWS line breaks here.
+    // doodad is the fucking array?
+    for (var prop in albums_json[doodad]) {
+        document.writeln('----' + prop + ': ' + albums_json[doodad][prop]);
+    }
+}
+
+document.writeln(marc_albums.length + ' albums written.');
